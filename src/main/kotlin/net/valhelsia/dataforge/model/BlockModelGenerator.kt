dce887d6579c3e697134cad8c72b4ca309d7d5c6
stal111 <stal111@gmx.de>
2024-10-13 13:09:56 +0200

    Add block getModelLocation helper

--- src/main/kotlin/net/valhelsia/dataforge/model/BlockModelGenerator.kt
+++ src/main/kotlin/net/valhelsia/dataforge/model/BlockModelGenerator.kt
@@ -1,10 +1,16 @@
 package net.valhelsia.dataforge.model
 
+import net.minecraft.core.Holder
 import net.minecraft.data.models.BlockModelGenerators
+import net.minecraft.data.models.model.ModelLocationUtils
+import net.minecraft.world.level.block.Block
 
 abstract class BlockModelGenerator(defaultGenerators: BlockModelGenerators) {
     val blockStateOutput = defaultGenerators.blockStateOutput
     val modelOutput = defaultGenerators.modelOutput
 
     abstract fun generate()
+
+    fun Holder<out Block>.getModelLocation(suffix: String = "") =
+        ModelLocationUtils.getModelLocation(this.value(), suffix)
 }
